b2b00db88ec26d91fe64e9981fe81535f9ccea2d
Michael-Schober <40370288+Michael-Schober@users.noreply.github.com>
2020-07-13 14:08:29 +0200

    fixed timing issue, no more events needed

--- src/testlab/components/Menu.js
+++ src/testlab/components/Menu.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react';
 import {Menubar} from 'primereact/menubar';
 import { RefContext } from './Context';
-
-
+import { Link } from 'react-router-dom';
 
 class Menu extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-
-        this.gotMenuItems = this.gotMenuItems.bind(this)
-    }
-
-    componentDidMount(){
-        this.menuSub = this.context.uiBuilder.menuSubject.subscribe(m => this.gotMenuItems(m));
-    }
-
-    componentWillUnmount(){
-        this.menuSub.unsubscribe();
-    }
-
-    gotMenuItems(menuItems){
-        this.setState({menu: menuItems});
-    }
-
     render() { 
         return (
-            <Menubar model={this.state.menu ? this.state.menu : this.props.model} />
+            <RefContext.Consumer>
+            {value => 
+                <Menubar model={value.contentSafe.menuItems}>
+                    <Link to="/main/settings" >Link to Settings</Link>
+                </Menubar>
+            }
+            </RefContext.Consumer>
         );
     }
 }
-Menu.contextType=RefContext;
 export default Menu;
