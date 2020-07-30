8054e1f19eb238c76ed3b377d3bfbfb37931fa6b
Michael-Schober <40370288+Michael-Schober@users.noreply.github.com>
2020-07-30 11:12:11 +0200

    filter fetch on change and dropdown

--- src/component/dynamic/editors/linked/UIEditorLinked.js
+++ src/component/dynamic/editors/linked/UIEditorLinked.js
@@ -8,7 +8,7 @@ class UIEditorLinked extends Base {
 
     state = {
         suggestions: [],
-        options: []
+        options: [],
     }
 
     constructor(props){
@@ -16,12 +16,21 @@ class UIEditorLinked extends Base {
         this.data = this.props.data
     }
 
-    componentDidMount() {
+    componentDidMount(){
         this.sub = this.context.contentSafe.selectedDataRowChange.subscribe(this.newSelection.bind(this))
+        this.elem = this.autoC.panel.element;
+        this.elem.addEventListener("scroll", this.handleScroll.bind(this));
     }
 
-    componentWillUnmount() {
+    componentWillUnmount(){
+        this.elem.removeEventListener("scroll", this.handleScroll.bind(this));
         this.sub.unsubscribe();
+    }
+
+    handleScroll(){
+        if((this.elem.scrollHeight - this.elem.scrollTop - this.elem.clientHeight) <= 0){
+            console.log("end reached")
+        } 
     }
 
     newSelection(newSelection){
@@ -29,16 +38,7 @@ class UIEditorLinked extends Base {
             this.setState({selectedObject: newSelection});
         } else {
             this.setState({selectedObject: undefined})
-        }
-
-        
-    }
-
-    async doesElementExist(toCheck){
-        let foundObj = this.state.options.find(currentOpt => {
-            return currentOpt[this.data.cellEditor.clearColumns[0]] === toCheck[this.data.cellEditor.clearColumns[0]]
-        });
-        return foundObj ? foundObj : toCheck
+        }   
     }
 
     formatFetchRequest(fetchResponse){
@@ -55,43 +55,29 @@ class UIEditorLinked extends Base {
     }
 
     fetchData(filter){
-        let fetchPromise;
-        if(filter){
-            fetchPromise =  this.context.serverComm.fetchFilterdData(
+        this.context.serverComm.fetchFilterdData(
                 this.data.cellEditor.linkReference.dataProvider, 
                 filter, this.data.name)
-        } else {
-            fetchPromise =  this.context.serverComm.fetchDataFromProvider(
-                this.data.cellEditor.linkReference.dataProvider)
-        }
-        return fetchPromise
-            .then(response => response.json())
-            .then(this.formatFetchRequest.bind(this));
+        .then(response => response.json())
+        .then(this.formatFetchRequest.bind(this))
+        .then(fetchedData => {
+            this.setState({options: fetchedData, suggestions: fetchedData})
+        });
     }
 
     autoComplete(event){
         this.fetchData(event.query)
-            .then(fetchedData => {
-                this.setState({options: fetchedData, suggestions: fetchedData})
-            });
-            let elem = this.autoC.panel.element;
-            this.autoC.panel.element.addEventListener("scroll", () => {
-
-                if((elem.scrollHeight - elem.scrollTop - elem.clientHeight) <= 0){
-                    console.log("end reached")
-                } 
-            })
     }
 
     render(){ 
         return (
             <AutoComplete
-            ref= {r => this.autoC = r}
+                ref= {r => this.autoC = r}
                 dropdown={true}
                 completeMethod={this.autoComplete.bind(this)}
                 field={this.data.columnName}    
                 value={this.state.selectedObject}
-                suggestions={this.state.suggestions ? this.state.suggestions : []}
+                suggestions={this.state.suggestions}
                 onChange={x => this.setState({selectedObject: x.target.value})}
             />
         )
