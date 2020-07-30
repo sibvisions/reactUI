8807f684f19e740bc081d6fc0c539f3295149551
Michael-Schober <40370288+Michael-Schober@users.noreply.github.com>
2020-07-30 11:12:29 +0200

    populate on select

--- src/component/dynamic/editors/number/UIEditorNumber.js
+++ src/component/dynamic/editors/number/UIEditorNumber.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Base from '../../Base';
 import { InputNumber } from 'primereact/inputnumber';
+import { RefContext } from '../../../helper/Context';
 
 class UIEditorNumber extends Base{
 
+
+    componentDidMount() {
+        this.sub = this.context.contentSafe.selectedDataRowChange.subscribe(this.setContent.bind(this))
+    }
+
+    componentWillUnmount() {
+        this.sub.unsubscribe();
+    }
+
+    setContent(content){
+        console.log(this)
+        if(content[this.props.data.columnName]){
+            this.setState({selection: content[this.props.data.columnName]});
+        }
+    }
+
+
     render(){
         return(
-            <InputNumber />
+            <InputNumber 
+                value={this.state.selection}
+                onChange={x => this.setState({selection: x.target.value})}/>
         )
     }
 }
+UIEditorNumber.contextType = RefContext
 export default UIEditorNumber
