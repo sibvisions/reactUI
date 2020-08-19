b55d0593f4bfb81dec16ccba74994bc6b09027d9
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2020-08-19 16:07:18 +0200

    icons, colors
    custom icons
    positioning icon only
    parsing/styling rework
    icon & font color
    iconTextGap
    font helper class
    button fonts
    button background

--- src/component/dynamic/button/UIButton.js
+++ src/component/dynamic/button/UIButton.js
@@ -7,13 +7,15 @@ import { checkAlignments, mapFlex } from '../../helper/CheckAlignments';
 import { Size } from '../../helper/Size';
 import { toPx } from '../../helper/ToPx';
 import { Margins } from '../../layouts/layoutObj/Margins';
+import { UIFont } from '../../helper/UIFont';
 
 class UIButton extends Base {
 
-    splittedImageName;
-    index;
     btnIcon;
     btnIconPos;
+    btnFont;
+    iconSize;
+    customIcon = false;
 
     componentDidMount() {
         this.styleButton(this.button.children[0]);
@@ -21,6 +23,13 @@ class UIButton extends Base {
     }
 
     styleButton(btn) {
+        if (btn.classList.contains("p-button-icon-only")) {
+            for (let child of btn.children) {
+                if (child.classList.contains("p-button-text")) {
+                    child.remove()
+                }
+            }
+        }
         let alignments = mapFlex(checkAlignments(this.props))
         btn.style.setProperty('display', 'flex');
         if (this.props.horizontalTextPosition === 1) {
@@ -34,63 +43,99 @@ class UIButton extends Base {
     }
 
     styleChildren(btnChildren) {
-        console.log(this.splittedImageName)
         for (let child of btnChildren) {
-            if (child.classList.contains('fas') || child.classList.contains("p-button-icon-left")) {
-                if (this.index < 0) {
-                    child.style.setProperty('width', toPx(this.splittedImageName[1]));
-                    child.style.setProperty('height', toPx(this.splittedImageName[2]));
-                }
-                else {
-                    this.splittedImageName.forEach(prop => {
-                        if (prop.indexOf("size") >= 0) {
-                            let size = prop.substring(prop.indexOf('=')+1);
-                            child.style.setProperty('height', toPx(size))
-                            child.style.setProperty('width', toPx(size))
-                        }
-                        else if (prop.indexOf("color") >= 0) {
-                            let iconColor = prop.substring(prop.indexOf('=')+1);
-                            child.style.setProperty('color', iconColor)
-                        }  
-                    });
-                }
+            if (child.classList.contains('fas') || child.classList.contains(this.btnIcon)) {
+                    child.style.setProperty('width', toPx(this.iconSize.width));
+                    child.style.setProperty('height', toPx(this.iconSize.height));
+                    if (this.iconColor !== null) {
+                        child.style.setProperty('color', this.iconColor);
+                    }
+                    if (child.classList.contains(this.btnIcon)) {
+                        child.classList.add("custom-icon");
+                        child.style.setProperty('--icon', 'url(' + this.btnIcon + ')');
+                    }
+                    if (this.props.imageTextGap !== undefined) {
+                        let gapPos = this.getGapPos(this.props.horizontalTextPosition, this.props.verticalTextPosition);
+                        child.style.setProperty('margin-' + gapPos, toPx(this.props.imageTextGap));
+                    }
             }
-            console.log(child, child.classList.contains('icon'))
             child.style.setProperty('background-color', this.props["cellEditor.background"])
             child.style.setProperty('padding', 0)
         }
     }
 
-    setIcon(imageString) {
-        console.log(imageString)
-        if (imageString !== undefined) {
-            if (imageString.includes("FontAwesome")) {
-                this.index = imageString.indexOf(";")
-                if (this.index < 0) {
-                    let iconString = imageString.slice(imageString.indexOf('.')+1)
-                    this.splittedImageName = iconString.split(',');
-                    this.btnIcon = "fas fa-" + this.splittedImageName[0];
+    parseIconData(iconData) {
+        if (iconData !== undefined) {
+            let splittedIconData;
+            if (iconData.includes("FontAwesome")) {
+                let index = iconData.indexOf(";")
+                if (index < 0) {
+                    let iconString = iconData.slice(iconData.indexOf('.')+1)
+                    splittedIconData = iconString.split(',');
+                    this.btnIcon = "fas fa-" + splittedIconData[0];
+                    this.iconSize = new Size(splittedIconData[1], splittedIconData[2]);
                 }
                 else {
-                    let iconString = imageString.slice(imageString.indexOf('.')+1).split(',');
-                    this.splittedImageName = iconString[0].split(';');
-                    this.btnIcon = "fas fa-" + this.splittedImageName[0];
+                    let iconString = iconData.slice(iconData.indexOf('.')+1).split(',');
+                    splittedIconData = iconString[0].split(';');
+                    this.btnIcon = "fas fa-" + splittedIconData[0];
+                    splittedIconData.splice(splittedIconData, 1)
+                    let sizeFound = false;
+                    let colorFound = false;
+                    splittedIconData.forEach(prop => {
+                        if (prop.indexOf("size") >= 0) {
+                            this.iconSize = new Size(prop.substring(prop.indexOf('=')+1), prop.substring(prop.indexOf('=')+1));
+                            sizeFound = true;
+                        }
+                        else if (prop.indexOf("color") >= 0) {
+                            this.iconColor = prop.substring(prop.indexOf('=')+1);
+                            colorFound = true;
+                        }
+                    });
+                    if (!sizeFound) {
+                        this.iconSize = new Size(iconString[1], iconString[2]);
+                    }
+                    if (!colorFound) {
+                        if (this.props.foreground !== undefined) {
+                            this.iconColor = this.props.foreground;
+                        }
+                        else {
+                            this.iconColor = null;
+                        }
+                    }
                 }
             }
             else {
-                this.index = -1;
-                this.splittedImageName = imageString.split(',')
-                this.btnIcon = this.splittedImageName[0]
+                this.customIcon = true;
+                splittedIconData = iconData.split(',');
+                this.btnIcon = splittedIconData[0];
+                this.iconSize = new Size(splittedIconData[1], splittedIconData[2]);
+                this.iconColor = null;
             }
         }
     }
 
     setIconPos(hTextPos, vTextPos) {
         if (hTextPos === 0 || (hTextPos === 1 && vTextPos === 0)) {
-            this.btnIconPos = "right"
+            this.btnIconPos = "right";
         }
         else {
-            this.btnIconPos = "left"
+            this.btnIconPos = "left";
+        }
+    }
+
+    getGapPos(hTextPos, vTextPos) {
+        if (hTextPos === 0) {
+            return 'left'
+        }
+        else if (hTextPos === undefined) {
+            return 'right'
+        }
+        else if (hTextPos === 1 && vTextPos === 2) {
+            return 'bottom'
+        }
+        else if (hTextPos === 1 && vTextPos === 0) {
+            return 'top'
         }
     }
 
@@ -99,9 +144,15 @@ class UIButton extends Base {
         if (this.props.margins !== undefined) {
             margins = new Margins(this.props.margins.split(','))
         }
-        this.setIcon(this.props.image);
-        //this.setIcon(process.env.PUBLIC_URL + '/assets/sibvisionslogo.png,16,16');
+        this.parseIconData(this.props.image);
         this.setIconPos(this.props.horizontalTextPosition, this.props.verticalTextPosition);
+        if (this.props.font !== undefined) {
+            this.btnFont = new UIFont(this.props.font.split(','));
+        }
+        else {
+            this.btnFont = new UIFont(["Tahoma", '0', '11'])
+        }
+        console.log(this.btnFont)
         return (
             <div ref={r => this.button = r} style={this.props.layoutStyle}>
                 <Button
@@ -109,7 +160,19 @@ class UIButton extends Base {
                     label={this.props.text}
                     constraints={this.props.constraints}
                     onClick={() => this.context.serverComm.pressButton(this.props.name)}
-                    style={{paddingTop: margins.marginTop, paddingLeft: margins.marginLeft, paddingBottom: margins.marginBottom, paddingRight: margins.marginRight}}
+                    style={{
+                        background: this.props.background,
+                        borderColor: this.props.background,
+                        paddingTop: margins.marginTop, 
+                        paddingLeft: margins.marginLeft, 
+                        paddingBottom: margins.marginBottom, 
+                        paddingRight: margins.marginRight, 
+                        color: this.props.foreground, 
+                        fontFamily: this.btnFont.fontFamily,
+                        fontWeight: this.btnFont.fontWeight,
+                        fontStyle: this.btnFont.fontStyle,
+                        fontSize: this.btnFont.fontSize
+                    }}
                     icon={this.btnIcon}
                     iconPos={this.btnIconPos}
                 />
