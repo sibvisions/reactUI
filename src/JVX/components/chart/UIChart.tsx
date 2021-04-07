557171cc68dfb0529d19d3ae2b5c1bdd16bd9955
Immanuel Bauer <gimmix.at@gmail.com>
2021-04-07 13:24:58 +0200

    some UIChart refactoring and additional handling / mapping of chart types

--- src/JVX/components/chart/UIChart.tsx
+++ src/JVX/components/chart/UIChart.tsx
@@ -1,3 +1,5 @@
+// API docs for ChartJS Version used in Prime React - https://www.chartjs.org/docs/2.7.3/
+
 /** React imports */
 import React, {FC, useContext, useLayoutEffect, useMemo, useRef} from "react";
 
@@ -78,9 +80,27 @@ const UIChart: FC<IChart> = (baseProps) => {
      */
     const chartType = useMemo(() => {
         switch (props.chartStyle) {
-            case CHART_STYLES.LINES: case CHART_STYLES.AREA: return "line";
-            case CHART_STYLES.BARS: return "bar";
-            default: return "pie"
+            case CHART_STYLES.LINES: 
+            case CHART_STYLES.AREA: 
+            case CHART_STYLES.STEPLINES: 
+            case CHART_STYLES.STACKEDAREA: 
+            case CHART_STYLES.STACKEDPERCENTAREA: 
+                return "line";
+            case CHART_STYLES.BARS: 
+            case CHART_STYLES.STACKEDBARS: 
+            case CHART_STYLES.STACKEDPERCENTBARS: 
+            case CHART_STYLES.OVERLAPPEDBARS: 
+                return "bar";
+            case CHART_STYLES.HBARS: 
+            case CHART_STYLES.STACKEDHBARS: 
+            case CHART_STYLES.STACKEDPERCENTHBARS: 
+            case CHART_STYLES.OVERLAPPEDHBARS: 
+                return "horizontalBar";
+            case CHART_STYLES.RING: 
+                return "doughnut";
+            case CHART_STYLES.PIE:
+            default: 
+                return "pie";
         }
     },[props.chartStyle])
 
@@ -91,20 +111,20 @@ const UIChart: FC<IChart> = (baseProps) => {
      */
     const chartData = useMemo(() => {
         const singleColor = tinycolor.random().toHexString();
+        const { chartStyle, data, yColumnLabels } = props;
         const primeChart = {
-            labels: props.data.map(dataRow => dataRow[1]),
-            datasets: [
-                {
-                    label: props.yColumnLabels[0],
-                    data: props.data.map(dataRow => dataRow[0]),
-                    backgroundColor: props.chartStyle === CHART_STYLES.PIE ? props.data.map(() => tinycolor.random().toHexString()): singleColor,
-                    borderColor: props.chartStyle !== CHART_STYLES.PIE ? singleColor : undefined,
-                    fill: props.chartStyle === CHART_STYLES.AREA ? true : false,
-                    lineTension: 0,
-                    pointRadius: props.chartStyle === CHART_STYLES.LINES ? 6 : 0,
-                    pointHitRadius: props.chartStyle === CHART_STYLES.LINES ? 7 : 0,
-                }
-            ]
+            labels: data.map(dataRow => dataRow[1]),
+            datasets: [{
+                label: yColumnLabels[0],
+                data: data.map(dataRow => dataRow[0]),
+                backgroundColor: [CHART_STYLES.PIE, CHART_STYLES.RING].includes(chartStyle) ? 
+                    data.map(() => tinycolor.random().toHexString()) : singleColor,
+                borderColor: ![CHART_STYLES.PIE, CHART_STYLES.RING].includes(chartStyle) ? singleColor : undefined,
+                fill: [CHART_STYLES.AREA, CHART_STYLES.STACKEDAREA, CHART_STYLES.STACKEDPERCENTAREA].includes(chartStyle) ? 'origin' : false,
+                lineTension: 0,
+                pointRadius: [CHART_STYLES.LINES, CHART_STYLES.STEPLINES].includes(chartStyle) ? 6 : 0,
+                pointHitRadius: [CHART_STYLES.LINES, CHART_STYLES.STEPLINES].includes(chartStyle) ? 7 : 0,
+            }]
         }
         return primeChart
     },[props.data, props.chartStyle, props.yColumnLabels]);
@@ -114,12 +134,16 @@ const UIChart: FC<IChart> = (baseProps) => {
      * Returns the maximum value of the data
      * @returns max value of data
      */
-    const getMaxDataVal = () => {
-        let tempArray:Array<number> = [];
-        props.data.forEach(dataRow => {
-            tempArray.push(dataRow[0])
-        })
-        return Math.max(...tempArray)
+    const getMaxDataVal = () => {        
+        return Math.max(...props.data.map(dataRow => dataRow[0]))
+    }
+
+    /**
+     * Returns the minimum value of the data or 0
+     * @returns min value of data
+     */
+     const getMinDataVal = () => {        
+        return Math.min(0, ...props.data.map(dataRow => dataRow[0]))
     }
 
     /**
@@ -128,31 +152,60 @@ const UIChart: FC<IChart> = (baseProps) => {
      * @returns options for display
      */
     const options = (style:number) => {
-        if (style === CHART_STYLES.PIE)
+        if ([CHART_STYLES.PIE, CHART_STYLES.RING].includes(style)) {
             return {
-                    legend: {
-                        display: false
-                    }
+                legend: {
+                    display: false
+                }
             }
-        else {
+        } else {
+            let xAxes:any[] = [{
+                stacked: [
+                    CHART_STYLES.STACKEDAREA, 
+                    CHART_STYLES.STACKEDBARS, 
+                    CHART_STYLES.STACKEDHBARS,
+                    CHART_STYLES.STACKEDPERCENTAREA,
+                    CHART_STYLES.STACKEDPERCENTBARS,
+                    CHART_STYLES.STACKEDPERCENTHBARS,
+                ].includes(style),
+                ticks: {
+                    callback: (value:any) => `${value.substr(0, 10)}...` //truncate
+                }
+            }];
+
+            let yAxes:any[] = [{
+                stacked: [
+                    CHART_STYLES.STACKEDAREA, 
+                    CHART_STYLES.STACKEDBARS, 
+                    CHART_STYLES.STACKEDHBARS,
+                    CHART_STYLES.STACKEDPERCENTAREA,
+                    CHART_STYLES.STACKEDPERCENTBARS,
+                    CHART_STYLES.STACKEDPERCENTHBARS,
+                ].includes(style),
+                ticks: {
+                    min: getMinDataVal(),
+                    max: getMaxDataVal()
+                }
+            }];
+
+            if ([
+                CHART_STYLES.HBARS,
+                CHART_STYLES.STACKEDHBARS,
+                CHART_STYLES.STACKEDPERCENTHBARS,
+                CHART_STYLES.OVERLAPPEDHBARS,
+            ].includes(style)) {
+                const t = xAxes;
+                xAxes = yAxes;
+                yAxes = t;
+            }
+            
             return {
                 legend: {
                     position: 'bottom'
                 },
                 scales: {
-                    xAxes: [{
-                        ticks: {
-                            callback: function(value:any) {
-                                return value.substr(0, 10)+"...";//truncate
-                            },
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            min: 0,
-                            max: getMaxDataVal()
-                        }
-                    }]
+                    xAxes,
+                    yAxes
                 },
             }
         }
