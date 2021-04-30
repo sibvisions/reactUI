103033b42c309bfd37d1256b91373fac038f57af
Immanuel Bauer <gimmix.at@gmail.com>
2021-04-30 19:38:12 +0200

    refactored useButtonMouseImages hook

--- src/JVX/components/zhooks/useButtonMouseImages.tsx
+++ src/JVX/components/zhooks/useButtonMouseImages.tsx
@@ -1,108 +1,92 @@
-import {useContext, useLayoutEffect} from "react"
+import {useContext, useLayoutEffect, useCallback, useRef, useEffect} from "react"
 import { jvxContext } from "../../jvxProvider";
 import IconProps from "../compprops/IconProps";
 
-const useButtonMouseImages = (iconData?:IconProps, mousePressedIconData?:IconProps, mouseOverIconData?:IconProps, btnElement?:any) => {
+const isFAIcon = (iconName?:string) => {
+    return iconName?.includes('fa fa-');
+}
+
+const removeIcon = (elem:HTMLElement, iconName:string) => {
+    if (isFAIcon(iconName)) {
+        elem.classList.remove(iconName.substring(3));
+    } else {
+        elem.style.setProperty('--iconImage', '');
+    }
+}
+
+const useEventHandler = (target?: HTMLElement, event?: keyof HTMLElementEventMap, handler?: EventListener) => {
+    const targetRef = useRef<HTMLElement>();
+    const handlerRef = useRef<EventListener>();
+    const eventRef = useRef<keyof HTMLElementEventMap>();
+
+    useEffect(() => {
+        if (targetRef.current && handlerRef.current && eventRef.current) {
+            targetRef.current.removeEventListener(eventRef.current, handlerRef.current);
+        }
+        targetRef.current = target;
+        handlerRef.current = handler;
+        eventRef.current = event;
+        if (target && event && handler) {
+            target.addEventListener(event, handler);
+        }
+    }, [target, event, handler])
+}
+
+const useButtonMouseImages = (iconData?:IconProps, mousePressedIconData?:IconProps, mouseOverIconData?:IconProps, btnElement?:HTMLElement) => {
     /** Use context to gain access for contentstore and server methods */
     const context = useContext(jvxContext);
 
-    useLayoutEffect(() => {
-        const isFAIcon = (iconName?:string) => {
-            return iconName?.includes('fa fa-');
+    const containsIcon = useCallback((elem:HTMLElement, iconName:string) => {
+        return elem.classList.contains(iconName.substring(3)) || 
+            window.getComputedStyle(elem).getPropertyValue('--iconImage') === `url(${context.server.RESOURCE_URL + iconName})`
+    }, [context.server.RESOURCE_URL]);
+
+    const addIcon = useCallback((elem:HTMLElement, iconName:string) => {
+        if (isFAIcon(iconName)) {
+            elem.classList.add(iconName.substring(3));
+        } else {
+            elem.style.setProperty('--iconImage', `url(${context.server.RESOURCE_URL + iconName})`);
         }
+    }, [context.server.RESOURCE_URL]);
 
-        const containsIcon = (elem:HTMLElement, iconName:string) => {
-            return elem.classList.contains(iconName.substring(3)) || window.getComputedStyle(btnElement).getPropertyValue('--iconImage') === 'url(' + context.server.RESOURCE_URL + iconName + ')'
+    const iconElement = btnElement?.children[0] as HTMLElement;
+
+    const handleMouseImagePressed = useCallback(() => {
+        if (mouseOverIconData?.icon && containsIcon(iconElement, mouseOverIconData.icon)) {
+            removeIcon(iconElement, mouseOverIconData.icon)
+        } else if (containsIcon(iconElement, iconData!.icon!)) {
+            removeIcon(iconElement, iconData!.icon!)
         }
+        addIcon(iconElement, mousePressedIconData!.icon!)
+    }, [iconElement, iconData?.icon, mouseOverIconData?.icon, mousePressedIconData?.icon]);
 
-        const removeIcon = (elem:HTMLElement, iconName:string) => {
-            if (isFAIcon(iconName)) {
-                elem.classList.remove(iconName.substring(3));
-            }
-            else {
-                elem.style.setProperty('--iconImage', '');
-            }
+    const handleMouseImageReleased = useCallback(() => {
+        removeIcon(iconElement, mousePressedIconData!.icon!);
+        if (mouseOverIconData?.icon) {
+            addIcon(iconElement, mouseOverIconData.icon);
+        } else {
+            addIcon(iconElement, iconData!.icon!);
         }
+    }, [iconElement, iconData?.icon, mouseOverIconData?.icon, mousePressedIconData?.icon]);
 
-        const addIcon = (elem:HTMLElement, iconName:string) => {
-            if (isFAIcon(iconName)) {
-                elem.classList.add(iconName.substring(3));
-            }
-            else {
-                elem.style.setProperty('--iconImage', 'url(' + context.server.RESOURCE_URL + iconName + ')');
-            }
+    const handleMouseImageOver = useCallback(() => {
+        removeIcon(iconElement, iconData!.icon!);
+        addIcon(iconElement, mouseOverIconData!.icon!)
+    }, [iconElement, iconData?.icon, mouseOverIconData?.icon])
+
+    const handleMouseImageOut = useCallback(() => {
+        if (mousePressedIconData?.icon && containsIcon(iconElement, mousePressedIconData.icon)) {
+            removeIcon(iconElement, mousePressedIconData.icon);
+        } else if (mouseOverIconData?.icon) {
+            removeIcon(iconElement, mouseOverIconData?.icon)
         }
+        addIcon(iconElement, iconData!.icon!)
+    }, [iconElement, iconData?.icon, mouseOverIconData?.icon, mousePressedIconData?.icon]);
 
-        const handleMouseImagePressed = (elem:HTMLElement) => {
-            if (mouseOverIconData?.icon && containsIcon(elem, mouseOverIconData.icon)) {
-                removeIcon(elem, mouseOverIconData.icon)
-            }
-            else if (containsIcon(elem, iconData!.icon!)) {
-                removeIcon(elem, iconData!.icon!)
-            }
-            addIcon(elem, mousePressedIconData!.icon!)
-        }
 
-        const handleMouseImageReleased = (elem:HTMLElement) => {
-            removeIcon(elem, mousePressedIconData!.icon!);
-            if (mouseOverIconData?.icon) {
-                addIcon(elem, mouseOverIconData.icon);
-            }
-            else {
-                addIcon(elem, iconData!.icon!);
-            }
-        }
-
-        const handleMouseImageOver = (elem:HTMLElement) => {
-            removeIcon(elem, iconData!.icon!);
-            addIcon(elem, mouseOverIconData!.icon!)
-        }
-
-        const handleMouseImageOut = (elem:HTMLElement) => {
-            if (mousePressedIconData?.icon && containsIcon(elem, mousePressedIconData.icon)) {
-                removeIcon(elem, mousePressedIconData.icon);
-            }
-            else if (mouseOverIconData?.icon) {
-                removeIcon(elem, mouseOverIconData?.icon)
-            }
-            addIcon(elem, iconData!.icon!)
-        }
-
-        if (btnElement && iconData?.icon) {
-            const iconElement = btnElement.children[0] as HTMLElement;
-
-            if (mousePressedIconData?.icon) {
-                btnElement.addEventListener('mousedown', () => handleMouseImagePressed(iconElement));
-                btnElement.addEventListener('mouseup', () => handleMouseImageReleased(iconElement));
-            }
-
-            if (mouseOverIconData?.icon) {
-                btnElement.addEventListener('mouseover', () => handleMouseImageOver(iconElement));
-            }
-
-            if (mousePressedIconData?.icon || mouseOverIconData?.icon) {
-                btnElement.addEventListener('mouseout', () => handleMouseImageOut(iconElement));
-            }
-        }
-
-        return () => {
-            if (btnElement && iconData?.icon) {
-                const iconElement = btnElement.children[0] as HTMLElement;
-
-                if (mousePressedIconData?.icon) {
-                    btnElement.removeEventListener('mousedown', () => handleMouseImagePressed(iconElement));
-                    btnElement.removeEventListener('mouseup', () => handleMouseImageReleased(iconElement));
-                }
-
-                if (mouseOverIconData?.icon) {
-                    btnElement.removeEventListener('mouseover', () => handleMouseImageOver(iconElement));
-                }
-
-                if (mousePressedIconData?.icon || mouseOverIconData?.icon) {
-                    btnElement.removeEventListener('mouseout', () => handleMouseImageOut(iconElement));
-                }
-            }
-        }
-    },[btnElement, context.server])
+    useEventHandler(btnElement, 'mousedown', iconData?.icon && mousePressedIconData?.icon ? handleMouseImagePressed : undefined);
+    useEventHandler(btnElement, 'mouseup', iconData?.icon && mousePressedIconData?.icon ? handleMouseImageReleased : undefined);
+    useEventHandler(btnElement, 'mouseenter', iconData?.icon && mouseOverIconData?.icon ? handleMouseImageOver : undefined);
+    useEventHandler(btnElement, 'mouseleave', iconData?.icon && (mousePressedIconData?.icon || mouseOverIconData?.icon) ? handleMouseImageOut : undefined);
 }
 export default useButtonMouseImages
