92dc06e4592ad99ab179a151b5782e19e8a635b3
Immanuel Bauer <gimmix.at@gmail.com>
2021-05-12 22:12:48 +0200

    use offsetWidth & offsetHeight in sendOnLoadCallback instead of getBoundingClientRect -> fixes layout in popup

--- src/main/components/util/SendOnLoadCallback.ts
+++ src/main/components/util/SendOnLoadCallback.ts
@@ -42,7 +42,8 @@ export function sendOnLoadCallback(id: string, preferredSize:Dimension|undefined
         }
         else {
             /** Measure how big the component wants to be initially */
-            const prefSize:Dimension = {width: ref.getBoundingClientRect().width, height: ref.getBoundingClientRect().height};
+            //const prefSize:Dimension = {width: ref.getBoundingClientRect().width, height: ref.getBoundingClientRect().height};
+            const prefSize:Dimension = {width: ref.offsetWidth, height: ref.offsetHeight};
             checkedSize = checkSizes(prefSize, minSize, maxSize)
         }
         onLoadCallback(id, checkedSize, minSize, maxSize);
