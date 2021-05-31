ff98136775d967449fc27917d31893b07bf4015b
Immanuel Bauer <gimmix.at@gmail.com>
2021-05-31 18:38:01 +0200

    refactor UIFactory

--- src/main/factories/UIFactory.tsx
+++ src/main/factories/UIFactory.tsx
@@ -10,19 +10,12 @@ import { UIGroupPanel,
         UIPopupWrapper,
         UIScrollPanel,
         UISplitPanel,
-        UITabsetPanel,
-        IPanel,
-        IPopup,
-        ISplit,
-        ITabsetPanel } from '../components/panels'
+        UITabsetPanel } from '../components/panels'
 import { UIButton,
         UICheckBox,
         UIMenuButton,
         UIToggleButton,
-        UIRadioButton,
-        IButton,
-        IButtonSelectable,
-        IMenuButton } from "../components/buttons"
+        UIRadioButton } from "../components/buttons"
 import UILabel from "../components/label/UILabel";
 import Dummy from "../components/dummy";
 import { UIEditorCheckBox,
@@ -31,291 +24,16 @@ import { UIEditorCheckBox,
         UIEditorImage,
         UIEditorLinked,
         UIEditorNumber,
-        UIEditorText,
-        IEditor,
-        IEditorCheckBox,
-        IEditorChoice,
-        IEditorDate,
-        IEditorImage,
-        IEditorLinked,
-        IEditorNumber,
-        IEditorText } from "../components/editors"
-import UITable, { TableProps } from "../components/table/UITable";
+        UIEditorText } from "../components/editors"
+import UITable from "../components/table/UITable";
 import UIIcon from "../components/icon/UIIcon";
 import { UIPassword, UIText, UITextArea } from "../components/text"
-import UIChart, { IChart } from "../components/chart/UIChart";
-import UIGauge, { IGauge } from "../components/gauge/UIGauge";
-import { UIMapGoogle, UIMapOSM, IMap } from "../components/map"
+import UIChart from "../components/chart/UIChart";
+import UIGauge from "../components/gauge/UIGauge";
+import { UIMapGoogle, UIMapOSM } from "../components/map"
 import { UICustomComponentWrapper, ICustomComponentWrapper } from '../components/customComp/index'
-import UITree, { ITree } from "../components/tree/UITree";
-import { LengthBasedColumnDescription, NumericColumnDescription } from "../response";
+import UITree from "../components/tree/UITree";
 
-/**
- * Returns a Panel as component as popup or normal
- * @param props - properties sent by the server
- * @returns a Panel as component
- */
-export const createPanel: FC<IPopup|IPanel> = (props) => {
-    if (props.screen_modal_)
-        return <UIPopupWrapper {...props} render={<UIPanel {...props} key={props.id}/>} key={'PopupWrapper-' + props.id}/>
-    else
-        return <UIPanel {...props} key={props.id}/>
-}
-
-/**
- * Returns a GroupPanel as component as popup or normal
- * @param props - properties sent by the server
- * @returns a GroupPanel as component
- */
-export const createGroupPanel: FC<IPopup|IPanel> = (props) => {
-    if (props.screen_modal_)
-        return <UIPopupWrapper {...props} render={<UIGroupPanel {...props} key={props.id}/>} key={'PopupWrapper-' + props.id}/>
-    else
-        return <UIGroupPanel {...props} key={props.id}/>
-}
-
-/**
- * Returns a ScrollPanel as component as popup or normal
- * @param props - properties sent by the server
- * @returns a ScrollPanel as component
- */
-export const createScrollPanel: FC<IPopup|IPanel> = (props) => {
-    if (props.screen_modal_)
-        return <UIPopupWrapper {...props} render={<UIScrollPanel {...props} key={props.id}/>} key={'PopupWrapper-' + props.id}/>
-    else
-        return <UIScrollPanel {...props} key={props.id}/>
-}
-
-/**
- * Returns a SplitPanel as component
- * @param props - properties sent by the server
- * @returns a SplitPanel as component
- */
-export const createSplitPanel: FC<ISplit> = (props) => {
-    return <UISplitPanel {...props} key={props.id}/>
-}
-
-/**
- * Returns a Button as component
- * @param props - properties sent by the server
- * @returns a Button as component
- */
-export const createButton: FC<IButton> = (props) => {
-    return <UIButton {...props} key={props.id}/>
-}
-
-/**
- * Returns a ToggleButton as component
- * @param props - properties sent by the server
- * @returns a ToggleButton as component
- */
-export const createToggleButton: FC<IButtonSelectable> = (props) => {
-    return <UIToggleButton {...props} key={props.id}/>
-}
-
-/**
- * Returns a PopupMenuButton as component
- * @param props - properties sent by the server
- * @returns a PopupMenuButton as component
- */
-export const createPopupMenuButton: FC<IMenuButton> = (props) => {
-    return <UIMenuButton {...props} key={props.id}/>
-}
-
-/**
- * Returns a RadioButton as component
- * @param props - properties sent by the server
- * @returns a RadioButton as component
- */
-export const createRadioButton: FC<IButtonSelectable> = (props) => {
-    return <UIRadioButton {...props} key={props.id}/>
-}
-
-/**
- * Returns a CheckBox as component
- * @param props - properties sent by the server
- * @returns a CheckBox as component
- */
-export const createCheckBox: FC<IButtonSelectable> = (props) => {
-    return <UICheckBox {...props} key={props.id}/>
-}
-
-/**
- * Returns a Label as component
- * @param props - properties sent by the server
- * @returns a Label as component
- */
-export const createLabel: FC<BaseComponent> = (props) => {
-    return <UILabel {...props} key={props.id}/>
-}
-
-/**
- * Returns a Dummy as component
- * @param props - properties sent by the server
- * @returns a Dummy as component
- */
-export const createDummy: FC<BaseComponent> = (props) => {
-    return <Dummy {...props} key={props.id}/>
-}
-
-/**
- * Returns an ImageViewer as component
- * @param props - properties sent by the server
- * @returns an ImageViewer as component
- */
-export const createEditorImage: FC<IEditorImage> = (props) => {
-    return <UIEditorImage {...props} key={props.id} />
-}
-
-/**
- * Returns a TextCellEditor as component
- * @param props - properties sent by the server
- * @returns a TextCellEditor as component
- */
-export const createEditorText: FC<IEditorText> = (props) => {
-    return <UIEditorText {...props} key={props.id}/>
-}
-
-/**
- * Returns a NumberCellEditor as component
- * @param props - properties sent by the server
- * @returns a NumberCellEditor as component
- */
-export const createEditorNumber: FC<IEditorNumber> = (props) => {
-    return <UIEditorNumber {...props} key={props.id}/>
-}
-
-/**
- * Returns a DateCellEditor as component
- * @param props - properties sent by the server
- * @returns a DateCellEditor as component
- */
-export const createEditorDate: FC<IEditorDate> = (props) => {
-    return <UIEditorDate {...props} key={props.id}/>
-}
-
-/**
- * Returns a ChoiceCellEditor as component
- * @param props - properties sent by the server
- * @returns a ChoiceCellEditor as component
- */
-export const createEditorChoice: FC<IEditorChoice> = (props) => {
-    return <UIEditorChoice {...props} key={props.id}/>
-}
-
-/**
- * Returns a CheckBoxCellEditor as component
- * @param props - properties sent by the server
- * @returns a CheckBoxCellEditor as component
- */
-export const createEditorCheckbox: FC<IEditorCheckBox> = (props) => {
-    return <UIEditorCheckBox {...props} key={props.id}/>
-}
-
-/**
- * Returns a LinkedCellEditor as component
- * @param props - properties sent by the server
- * @returns a LinkedCellEditor as component
- */
-export const createEditorLinked: FC<IEditorLinked> = (props) => {
-    return <UIEditorLinked {...props} key={props.id}/>
-}
-
-/**
- * Returns a Table as component
- * @param props - properties sent by the server
- * @returns a Table as component
- */
-export const createTable: FC<TableProps> = (props) => {
-    return <UITable {...props} key={props.id}/>
-}
-
-/**
- * Returns an Icon as component
- * @param props - properties sent by the server
- * @returns an Icon as component
- */
-export const createIcon: FC<BaseComponent> = (props) => {
-    return <UIIcon {...props} key={props.id}/>
-}
-
-/**
- * Returns a TextField as component
- * @param props - properties sent by the server
- * @returns a TextField as component
- */
-export const createTextField: FC<BaseComponent> = (props) => {
-    return <UIText {...props} key={props.id}/>
-}
-
-/**
- * Returns a TextArea as component
- * @param props - properties sent by the server
- * @returns a TextArea as component
- */
-export const createTextArea: FC<BaseComponent> = (props) => {
-    return <UITextArea {...props} key={props.id}/>
-}
-
-/**
- * Returns a PasswordField as component
- * @param props - properties sent by the server
- * @returns a PasswordField as component
- */
-export const createPassword: FC<BaseComponent> = (props) => {
-    return <UIPassword {...props} key={props.id}/>
-}
-
-/**
- * Returns a TabsetPanel as component as popup or normal
- * @param props - properties sent by the server
- * @returns a TabsetPanel as component as popup or normal
- */
-export const createTabsetPanel: FC<ITabsetPanel|IPopup> = (props) => {
-    if (props.screen_modal_)
-        return <UIPopupWrapper {...props} render={<UITabsetPanel {...props} key={props.id}/>} key={'PopupWrapper-' + props.id}/>
-    else
-        return <UITabsetPanel {...props} key={props.id}/>
-}
-
-/**
- * Returns a Chart as component
- * @param props - properties sent by the server
- * @returns a Chart as component
- */
-export const createChart: FC<IChart> = (props) => {
-    return <UIChart {...props} key={props.id}/>
-}
-
-/**
- * Returns a Gauge as component
- * @param props - properties sent by the server
- * @returns a tree as component
- */
- export const createGauge: FC<IGauge> = (props) => {
-    return <UIGauge {...props} key={props.id}/>
-}
-
-/**
- * Returns a Map either Google Maps or OpenStreetMaps as component
- * @param props - properties sent by the server
- * @returns a Map either Google Maps or OpenStreetMaps as component
- */
-export const createMap: FC<IMap> = (props) => {
-    if (props.tileProvider === "google")
-        return <UIMapGoogle {...props} key={props.id}/>
-    else
-        return <UIMapOSM {...props} key={props.id}/>
-}
-
-/**
- * Returns a Tree as component
- * @param props - properties sent by the server
- * @returns a tree as component
- */
-export const createTree: FC<ITree> = (props) => {
-    return <UITree {...props} key={props.id}/>
-}
 
 /**
  * Returns a CustomComponent wrapped in a Wrapper as component
@@ -328,76 +46,97 @@ export const createCustomComponentWrapper: FC<ICustomComponentWrapper> = (props,
 }
 
 /**
- * Decides which CellEditor function should be used
+ * Decides which CellEditor should be used
  * @param props - properties sent by the server
  */
-export const createEditor: FC<IEditor|LengthBasedColumnDescription|NumericColumnDescription> = ( props ) => {
+const Editor = (props: any) => {
     if(props.cellEditor) {
         if(props.cellEditor.className === "ImageViewer"){
-            return createEditorImage((props as IEditorImage));
+            return <UIEditorImage {...props} />;
         }
         else if(props.cellEditor.className === "TextCellEditor"){
-            return createEditorText((props as IEditorText));
+            return <UIEditorText {...props} />
         }
         else if (props.cellEditor.className === "NumberCellEditor") {
-            return createEditorNumber((props as IEditorNumber));
+            return <UIEditorNumber {...props} />
         }
         else if (props.cellEditor.className === "DateCellEditor") {
-            return createEditorDate((props as IEditorDate));
+            return <UIEditorDate {...props} />
         }
         else if (props.cellEditor.className === "ChoiceCellEditor") {
-            return createEditorChoice((props as IEditorChoice));
+            return <UIEditorChoice {...props} />
         }
         else if (props.cellEditor.className === "CheckBoxCellEditor") {
-            return createEditorCheckbox((props as IEditorCheckBox));
+            return <UIEditorCheckBox {...props} />
         }
         else if (props.cellEditor.className === "LinkedCellEditor") {
-            return createEditorLinked((props as IEditorLinked));
+            return <UIEditorLinked {...props} />
         }
         else{
-            return createDummy(props as IEditor)
+            return <Dummy {...props} />
         }
     } else {
-        return createDummy(props as IEditor)
+        return <Dummy {...props} />
     }
 }
 
 /**
- * Map to get the correct function to build a component for className
+ * Decides which CellEditor should be used
+ * @param props - properties sent by the server
  */
-const classNameMapper = new Map<string, Function>()
-    .set("Panel", createPanel)
-    .set("GroupPanel", createGroupPanel)
-    .set("ScrollPanel", createScrollPanel)
-    .set("SplitPanel", createSplitPanel)
-    .set("Button", createButton)
-    .set("ToggleButton", createToggleButton)
-    .set("PopupMenuButton", createPopupMenuButton)
-    .set("RadioButton", createRadioButton)
-    .set("CheckBox", createCheckBox)
-    .set("Label", createLabel)
-    .set("Editor", createEditor)
-    .set("Table", createTable)
-    .set("Icon", createIcon)
-    .set("TextField", createTextField)
-    .set("TextArea", createTextArea)
-    .set("PasswordField", createPassword)
-    .set("TabsetPanel", createTabsetPanel)
-    .set("Chart", createChart)
-    .set("Map", createMap)
-    .set("Tree", createTree)
-    .set("Gauge", createGauge)
+export function createEditor(props: any) {
+    return <Editor {...props} />
+}
 
 /**
- * Returns the function to build the component
- * @param component - the component to build
- * @returns the function to build the component
+ * Wraps the JSX Element with a UIPopupWrapper if the element.props.screen_modal_ is set `true`
+ * @param element - The JSX Element to wrap
+ * @returns The original or wrapped JSX Element
  */
-export const componentHandler = (component: BaseComponent) => {
-    const builder = classNameMapper.get(component.className as string);
-    if(builder) {
-        return builder(component);
+const maybePopup = (element: JSX.Element) =>
+    element.props.screen_modal_ 
+        ? <UIPopupWrapper {...element.props} render={element} key={'PopupWrapper-' + element.props.id}/> 
+        : element
+
+/**
+ * Map to get the correct function to build a component for className
+ */
+const componentsMap = new Map<string, React.ComponentType<any>>()
+    .set("Panel", props => maybePopup(<UIPanel {...props} />))
+    .set("GroupPanel", props => maybePopup(<UIGroupPanel {...props} />))
+    .set("ScrollPanel", props => maybePopup(<UIScrollPanel {...props} />))
+    .set("SplitPanel", props => <UISplitPanel {...props} />)
+    .set("Button", props => <UIButton {...props} />)
+    .set("ToggleButton", props => <UIToggleButton {...props} />)
+    .set("PopupMenuButton", props => <UIMenuButton {...props} />)
+    .set("RadioButton", props => <UIRadioButton {...props} />)
+    .set("CheckBox", props => <UICheckBox {...props} />)
+    .set("Label", props => <UILabel {...props} />)
+    .set("Editor", props => <Editor {...props} />)
+    .set("Table", props => <UITable {...props} />)
+    .set("Icon", props => <UIIcon {...props} />)
+    .set("TextField", props => <UIText {...props} />)
+    .set("TextArea", props => <UITextArea {...props} />)
+    .set("PasswordField", props => <UIPassword {...props} />)
+    .set("TabsetPanel", props => maybePopup(<UITabsetPanel {...props} />))
+    .set("Chart", props => <UIChart {...props} />)
+    .set("Map", props => props.tileProvider === "google"
+        ? <UIMapGoogle {...props} />
+        : <UIMapOSM {...props} />
+    )
+    .set("Tree", props => <UITree {...props} />)
+    .set("Gauge", props => <UIGauge {...props} />)
+
+/**
+ * Returns the JSXElement for the given base component
+ * @param baseComponent - the basecomponent to build
+ * @returns the resulting JSXElement
+ */
+export const componentHandler = (baseComponent: BaseComponent) => {
+    const Comp = componentsMap.get(baseComponent.className as string);
+    if(Comp) {
+        return <Comp {...baseComponent} key={baseComponent.id} />;
     } else {
-        return <div> </div>
+        return <div />
     }
 }
