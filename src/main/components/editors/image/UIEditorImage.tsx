cfad72c9faaa120d411d7b67587b7ae36edecec9
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2021-10-13 14:51:41 +0200

    login, sessionexp style tweaks, imgeditor fix when image couldn't be loaded and selectedRow changed

--- src/main/components/editors/image/UIEditorImage.tsx
+++ src/main/components/editors/image/UIEditorImage.tsx
@@ -61,8 +61,10 @@ const UIEditorImage: FC<IEditorImage> = (props) => {
     /** Hook for MouseListener */
     useMouseListener(props.name, wrapRef.current ? wrapRef.current : undefined, props.eventMouseClicked, props.eventMousePressed, props.eventMouseReleased);
 
+    const popupMenu = usePopupMenu(props);
+
     useEffect(() => {
-        if (!props.cellEditor.defaultImageName) {
+        if (!props.cellEditor.defaultImageName || !selectedRow) {
             const prefSize:Dimension = {width: 0, height: 0}
             if (props.preferredSize) {
                 const parsedSize = parsePrefSize(props.preferredSize) as Dimension
@@ -72,13 +74,16 @@ const UIEditorImage: FC<IEditorImage> = (props) => {
             if (onLoadCallback)
                 sendOnLoadCallback(id, prefSize, parseMaxSize(props.maximumSize), parseMinSize(props.minimumSize), undefined, onLoadCallback)
         }
-    },[onLoadCallback, id, props.cellEditor.defaultImageName, props.preferredSize, props.maximumSize, props.minimumSize])
+    },[onLoadCallback, id, props.cellEditor.defaultImageName, props.preferredSize, props.maximumSize, props.minimumSize]);
 
     /**
      * When the image is loaded, measure the image and then report its preferred-, minimum-, maximum and measured-size to the layout
      * @param event - image load event
      */
     const imageLoaded = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        if ((event.target as HTMLElement).style.display === "none") {
+            (event.target as HTMLElement).style.removeProperty("display");
+        }
         const prefSize:Dimension = {width: 0, height: 0}
         if(props.preferredSize) {
             const parsedSize = parsePrefSize(props.preferredSize) as Dimension
@@ -90,33 +95,34 @@ const UIEditorImage: FC<IEditorImage> = (props) => {
             prefSize.width = event.currentTarget.width;
         }
 
-        if(onLoadCallback) {
+        if (onLoadCallback) {
             sendOnLoadCallback(id, prefSize, parseMaxSize(props.maximumSize), parseMinSize(props.minimumSize), undefined, onLoadCallback);
         }   
     }
 
-    return(
-        <span 
-            ref={wrapRef} 
-            className="rc-editor-image" 
-            style={{...layoutStyle, overflow: "hidden"}} 
+    return (
+        <span
+            ref={wrapRef}
+            className="rc-editor-image"
+            style={{ ...layoutStyle, overflow: "hidden" }}
             aria-label={props.ariaLabel}
             onFocus={props.eventFocusGained ? () => onFocusGained(props.name, context.server) : undefined}
             onBlur={props.eventFocusLost ? () => onFocusLost(props.name, context.server) : undefined}
             tabIndex={selectedRow || props.cellEditor.defaultImageName ? (props.tabIndex ? props.tabIndex : 0) : undefined}
         >
             <Tooltip target={!isCellEditor ? "#" + props.name : undefined} />
-            <img
-                id={!isCellEditor ? props.name : undefined}
-                className={imageStyle}
-                //style={imageStyle.img}
-                src={selectedRow ? "data:image/jpeg;base64," + selectedRow : context.server.RESOURCE_URL + props.cellEditor.defaultImageName}
-                alt="could not be loaded"
-                onLoad={imageLoaded}
-                onError={e => (e.target as HTMLImageElement).style.display = 'none'}
-                data-pr-tooltip={props.toolTipText}
-                {...usePopupMenu(props)}
-            />
+            {(selectedRow || props.cellEditor.defaultImageName) &&
+                <img
+                    id={!isCellEditor ? props.name : undefined}
+                    className={imageStyle}
+                    //style={imageStyle.img}
+                    src={selectedRow ? "data:image/jpeg;base64," + selectedRow : context.server.RESOURCE_URL + props.cellEditor.defaultImageName}
+                    alt="could not be loaded"
+                    onLoad={imageLoaded}
+                    onError={e => (e.target as HTMLImageElement).style.display = 'none'}
+                    data-pr-tooltip={props.toolTipText}
+                {...popupMenu}
+                />}
         </span>
 
     )
