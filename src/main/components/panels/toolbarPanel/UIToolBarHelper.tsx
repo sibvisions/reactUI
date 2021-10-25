9589b9f15f04ad0c6588dd33a1265018f8a2f54f
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2021-10-25 09:59:39 +0200

    #207

--- src/main/components/panels/toolbarPanel/UIToolBarHelper.tsx
+++ src/main/components/panels/toolbarPanel/UIToolBarHelper.tsx
@@ -1,5 +1,5 @@
 /** React imports */
-import React, { FC, useContext, useMemo, useRef } from "react";
+import React, { FC, useContext, useLayoutEffect, useMemo, useRef } from "react";
 
 /** Hook imports */
 import { useProperties, useComponents, useLayoutValue, useMouseListener } from "../../zhooks";
@@ -61,6 +61,23 @@ const UIToolBarHelper: FC<IToolBarHelper> = (baseProps) => {
         }
     }
 
+    useLayoutEffect(() => {
+        if (context.contentStore.getChildren(id, props.className).size === 0 && props.className === COMPONENT_CLASSNAMES.TOOLBARHELPERMAIN) {
+            panelReportSize(
+                id, 
+                "P", 
+                {height: 0, width: 0},
+                props.className,
+                {height: 0, width: 0}, 
+                props.preferredSize, 
+                props.minimumSize, 
+                props.maximumSize, 
+                onLoadCallback
+            )
+        }
+
+    }, [onLoadCallback, id, props.preferredSize, props.maximumSize, props.minimumSize])
+
     /** 
      * The component reports its preferred-, minimum-, maximum and measured-size to the layout
      * In panels, this method will be passed to the layouts
@@ -81,47 +98,51 @@ const UIToolBarHelper: FC<IToolBarHelper> = (baseProps) => {
 
     return (
         <>
-            <Tooltip target={"#" + props.name} />
-            <div
-                className={concatClassnames(
-                    props.className === COMPONENT_CLASSNAMES.TOOLBARHELPERMAIN ? "rc-toolbar" : "rc-panel",
-                    props.className === COMPONENT_CLASSNAMES.TOOLBARHELPERMAIN && props.isNavTable ? getTBPosClassName(props.constraints) : ""
-                )}
-                ref={panelRef}
-                id={props.name}
-                style={props.screen_modal_ ? {
-                    height: prefSize?.height,
-                    width: prefSize?.width,
-                    ...(props.backgroundImage ? { '--backgroundImage': `url(${context.server.RESOURCE_URL + props.backgroundImage.split(',')[0]})` } : {}),
-                    display: filteredComponents.length === 0 ? "none" : ""
-                } : {
-                    ...layoutStyle,
-                    backgroundColor: props.background,
-                    ...(props.backgroundImage ? { '--backgroundImage': `url(${context.server.RESOURCE_URL + props.backgroundImage.split(',')[0]})` } : {}),
-                    display: filteredComponents.length === 0 ? "none" : ""
-                }}
-                data-pr-tooltip={props.toolTipText}>
-                <Layout
-                    id={id}
-                    className={props.className}
-                    layoutData={props.layoutData}
-                    layout={props.layout}
-                    preferredSize={parsePrefSize(props.preferredSize)}
-                    minimumSize={parseMinSize(props.minimumSize)}
-                    maximumSize={parseMaxSize(props.maximumSize)}
-                    popupSize={parsePrefSize(props.screen_size_)}
-                    reportSize={reportSize}
-                    compSizes={componentSizes ? new Map([...componentSizes].filter((v, k) => !v[0].includes("-tb"))) : undefined}
-                    components={filteredComponents}
-                    style={panelGetStyle(
-                        false,
-                        layoutStyle,
-                        prefSize,
-                        props.screen_modal_,
-                        props.screen_size_
-                    )}
-                    parent={props.parent} />
-            </div>
+            {context.contentStore.getChildren(id, props.className).size > 0 &&
+                <>
+                    <Tooltip target={"#" + props.name} />
+                    <div
+                        className={concatClassnames(
+                            props.className === COMPONENT_CLASSNAMES.TOOLBARHELPERMAIN ? "rc-toolbar" : "rc-panel",
+                            props.className === COMPONENT_CLASSNAMES.TOOLBARHELPERMAIN && props.isNavTable ? getTBPosClassName(props.constraints) : ""
+                        )}
+                        ref={panelRef}
+                        id={props.name}
+                        style={props.screen_modal_ ? {
+                            height: prefSize?.height,
+                            width: prefSize?.width,
+                            ...(props.backgroundImage ? { '--backgroundImage': `url(${context.server.RESOURCE_URL + props.backgroundImage.split(',')[0]})` } : {}),
+                            display: filteredComponents.length === 0 ? "none" : ""
+                        } : {
+                            ...layoutStyle,
+                            backgroundColor: props.background,
+                            ...(props.backgroundImage ? { '--backgroundImage': `url(${context.server.RESOURCE_URL + props.backgroundImage.split(',')[0]})` } : {}),
+                            display: filteredComponents.length === 0 ? "none" : ""
+                        }}
+                        data-pr-tooltip={props.toolTipText}>
+                        <Layout
+                            id={id}
+                            className={props.className}
+                            layoutData={props.layoutData}
+                            layout={props.layout}
+                            preferredSize={parsePrefSize(props.preferredSize)}
+                            minimumSize={parseMinSize(props.minimumSize)}
+                            maximumSize={parseMaxSize(props.maximumSize)}
+                            popupSize={parsePrefSize(props.screen_size_)}
+                            reportSize={reportSize}
+                            compSizes={componentSizes ? new Map([...componentSizes].filter((v, k) => !v[0].includes("-tb"))) : undefined}
+                            components={filteredComponents}
+                            style={panelGetStyle(
+                                false,
+                                layoutStyle,
+                                prefSize,
+                                props.screen_modal_,
+                                props.screen_size_
+                            )}
+                            parent={props.parent} />
+                    </div>
+                </>
+            }
         </>
     )
 }
