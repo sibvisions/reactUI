1d52baf55e8d805032f2ed42a9ab8c687d6c0cbf
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-03-04 11:32:40 +0100

    bright and dark button not on transparent background

--- src/main/components/buttons/radiobutton/UIRadioButton.tsx
+++ src/main/components/buttons/radiobutton/UIRadioButton.tsx
@@ -92,7 +92,15 @@ const UIRadioButton: FC<IButtonSelectable> = (baseProps) => {
                     className={concatClassnames(
                         "p-radiobutton-label",
                         btnStyle.style.color ? 'textcolor-set' : '',
-                        btnStyle.borderPainted && tinycolor(btnStyle.style.background?.toString()).isDark() ? "bright-button" : "dark-button",
+                        btnStyle.style.background !== "transparent" 
+                        ? 
+                            btnStyle.borderPainted && tinycolor(btnStyle.style.background?.toString()).isDark() 
+                            ? 
+                                "bright-button" 
+                            : 
+                                "dark-button"
+                        :
+                            "",
                         props.eventMousePressed ? "mouse-pressed-event" : ""
                         )} 
                     htmlFor={props.id} 
