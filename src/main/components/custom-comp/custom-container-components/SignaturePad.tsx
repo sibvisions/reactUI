37f7f876faf1a6c4eaf272d4048164e5823d997c
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-05-05 11:38:14 +0200

    #306

--- src/main/components/custom-comp/custom-container-components/SignaturePad.tsx
+++ src/main/components/custom-comp/custom-container-components/SignaturePad.tsx
@@ -1,12 +1,16 @@
-import React, { FC } from "react"
+import React, { FC, useContext, useEffect, useState } from "react"
 import SignatureCanvas from 'react-signature-canvas'
+import { appContext } from "../../../AppProvider"
 import { concatClassnames } from "../../../util"
 import BaseComponent from "../../../util/types/BaseComponent"
 
 const SignaturePad:FC<BaseComponent> = (props) => {
+    /** Use context to gain access for contentstore and server methods */
+    const context = useContext(appContext);
+
     return (
         <div className={concatClassnames("rc-signature-pad", props.style)} style={{ height: 200, width: 400, border: "1px solid #000" }}>
-            <SignatureCanvas canvasProps={{height: 200, width: 400, className: 'sigCanvas'}} />
+            <SignatureCanvas penColor={context.appSettings.applicationMetaData.applicationColorScheme.value === "dark" ? "white" : "black"} canvasProps={{height: 200, width: 400, className: 'sigCanvas'}} />
         </div>
     )
 }
