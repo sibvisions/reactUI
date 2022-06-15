4f22aace6881acebae81f5393127ab969e1b79d4
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-06-15 13:37:58 +0200

    babel plugin nullish test

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,11 @@ module.exports = () => {
                                 '@babel/preset-typescript',
                                 '@babel/preset-react'
                             ],
-                            plugins: ['@babel/plugin-proposal-class-properties']
+                            plugins: [
+                                '@babel/plugin-proposal-class-properties',
+                                '@babel/plugin-proposal-nullish-coalescing-operator',
+                                '@babel/plugin-proposal-optional-chaining'
+                            ]
                         }
                     }
                 },
