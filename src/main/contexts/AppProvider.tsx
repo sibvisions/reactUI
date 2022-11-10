65ac97e7be5690ce69c402ab922d7d598629f6b9
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-11-10 13:23:17 +0100

    #344

--- src/main/contexts/AppProvider.tsx
+++ src/main/contexts/AppProvider.tsx
@@ -560,8 +560,8 @@ const AppProvider: FC<ICustomContent> = (props) => {
             else {
                 contextState.server = new Server(contextState.contentStore, contextState.subscriptions, contextState.appSettings, history);
                 contextState.contentStore.setServer(contextState.server);
-                if (history.location.pathname.includes("#/home")) {
-                    contextState.server.linkOpen = history.location.pathname.replaceAll("/", "").substring(indexOfEnd(history.location.pathname, "#home") - 1);
+                if (history.location.pathname.includes("/home/")) {
+                    contextState.server.linkOpen = history.location.pathname.replaceAll("/", "").substring(indexOfEnd(history.location.pathname, "home") - 1);
                 }
                 contextState.api.setServer(contextState.server);
                 contextState.subscriptions.setServer(contextState.server);
