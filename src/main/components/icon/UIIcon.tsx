23d9130a4f453ce31d111f2f7b75256d2e09da99
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2023-01-19 15:36:45 +0100

    FS#8112

--- src/main/components/icon/UIIcon.tsx
+++ src/main/components/icon/UIIcon.tsx
@@ -154,7 +154,7 @@ const UIIcon: FC<BaseComponent & IExtendableIcon> = (baseProps) => {
                 props.focusable === false ? "no-focus-rect" : "",
                 props.style
             )}
-            style={{...layoutStyle, ...compStyle, overflow: "hidden", justifyContent: alignments.ha, alignItems: alignments.va}}
+            style={{...layoutStyle, ...compStyle, justifyContent: alignments.ha, alignItems: alignments.va}}
             tabIndex={getTabIndex(props.focusable, props.tabIndex)}
         >
             <Tooltip target={"#" + props.name} />
