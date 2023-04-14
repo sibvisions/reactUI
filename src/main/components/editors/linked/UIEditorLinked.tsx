53d8dc615d6f925e25cf68d880580aaf387a7448
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2023-04-14 11:46:29 +0200

    FS#8286

--- src/main/components/editors/linked/UIEditorLinked.tsx
+++ src/main/components/editors/linked/UIEditorLinked.tsx
@@ -695,8 +695,7 @@ const UIEditorLinked: FC<IEditorLinked & IExtendableLinkedEditor> = (props) => {
             sendSelectRequest(-1, null)
             sendSetValues(props.dataRow, props.name, colNames, props.columnName, null, props.context.server, props.topbar, -1);
         }
-
-        if (props.cellEditor.validationEnabled === false) {
+        else if (props.cellEditor.validationEnabled === false) {
             let tempArray = [];
             for (let i = 0; i < colNames.length; i++) {
                 if (colNames[i] !== props.columnName) {
