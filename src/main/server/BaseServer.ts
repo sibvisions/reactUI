d28e68cc50321c628aec7fa3c162002c58c22f44
Immanuel Bauer <gimmix.at@gmail.com>
2025-02-03 19:33:50 +0100

    ad exception for Filter requests when checking if data provider already exists

--- src/main/server/BaseServer.ts
+++ src/main/server/BaseServer.ts
@@ -282,8 +282,13 @@ export default abstract class BaseServer {
                                     return
                                 }
             
-                                // If the dataprovider doesn't exist yet and it is not under the missingdatafetches array. It doesn't exist and the request is not to be sent
-                                if (!this.contentStore.dataBooks.get(dataProviderScreenName)?.has(request.dataProvider) && !this.missingDataFetches.includes(request.dataProvider)) {
+                                // If the dataprovider doesn't exist yet and it is not under the missingdatafetches array 
+                                // it doesn't exist and the request is not to be sent unless is a Filter Request
+                                if (
+                                    endpoint != REQUEST_KEYWORDS.FILTER &&
+                                    !this.contentStore.dataBooks.get(dataProviderScreenName)?.has(request.dataProvider) && 
+                                    !this.missingDataFetches.includes(request.dataProvider)
+                                ) {
                                     reject("Dataprovider doesn't exist: " + request.dataProvider);
                                     return
                                 }
